5db39a9e0ddb3b0a8640e191afe151a04f7ee1e5
Revenko Grisha <grisha@revenko.org>
2023-02-07 23:41:19 +0300

    Added methods overloads with Collider2D

--- Assets/UnityTools/Tools.cs
+++ Assets/UnityTools/Tools.cs
@@ -37,6 +37,35 @@ namespace UnityTools
         }
 
         /// <summary>
+        /// Invokes given methods if Collider container has component requested as generic type.
+        /// Component of given generic type is used as argument for methods.
+        /// </summary>
+        public static bool InvokeIfNotNull<T>(Collider2D container, params Action<T>[] handlers)
+        {
+            var component = container.GetComponent<T>();
+            var isNotComponentNull = component != null;
+            if (isNotComponentNull)
+                foreach (var handler in handlers)
+                    handler?.Invoke(component);
+
+            return isNotComponentNull;
+        }
+
+        /// <summary>
+        /// Invokes given methods if Collider container has component requested as generic type.
+        /// </summary>
+        public static bool InvokeIfNotNull<T>(Collider2D container, params Action[] handlers)
+        {
+            var component = container.GetComponent<T>();
+            var isNotComponentNull = component != null;
+            if (isNotComponentNull)
+                foreach (var handler in handlers)
+                    handler?.Invoke();
+
+            return isNotComponentNull;
+        }
+
+        /// <summary>
         /// Takes other methods via Action param and invokes each method
         /// with given argument.
         /// </summary>
