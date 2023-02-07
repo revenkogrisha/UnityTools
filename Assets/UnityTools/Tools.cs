af210cc12983fe34478ac3b25834beef64dbf181
Revenko Grisha <grisha@revenko.org>
2023-02-07 23:42:51 +0300

    Add methods overloads with Collision argument

--- Assets/UnityTools/Tools.cs
+++ Assets/UnityTools/Tools.cs
@@ -66,6 +66,37 @@ namespace UnityTools
         }
 
         /// <summary>
+        /// Invokes given methods if Collider container has component requested as generic type.
+        /// Component of given generic type is used as argument for methods.
+        /// </summary>
+        public static bool InvokeIfNotNull<T>(Collision collision, params Action<T>[] handlers)
+        {
+            var container = collision.collider;
+            var component = container.GetComponent<T>();
+            var isNotComponentNull = component != null;
+            if (isNotComponentNull)
+                foreach (var handler in handlers)
+                    handler?.Invoke(component);
+
+            return isNotComponentNull;
+        }
+
+        /// <summary>
+        /// Invokes given methods if Collider container has component requested as generic type.
+        /// </summary>
+        public static bool InvokeIfNotNull<T>(Collision collision, params Action[] handlers)
+        {
+            var container = collision.collider;
+            var component = container.GetComponent<T>();
+            var isNotComponentNull = component != null;
+            if (isNotComponentNull)
+                foreach (var handler in handlers)
+                    handler?.Invoke();
+
+            return isNotComponentNull;
+        }
+
+        /// <summary>
         /// Takes other methods via Action param and invokes each method
         /// with given argument.
         /// </summary>
