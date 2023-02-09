92dc946f87f7099764d93f1d582a712fc8d8a9ab
Revenko Grisha <grisha@revenko.org>
2023-02-09 23:45:36 +0300

    Added TryGetComponent(s)InChildren extension methods

--- Assets/UnityTools/ComponentExtensions.cs
+++ Assets/UnityTools/ComponentExtensions.cs
@@ -16,8 +16,21 @@ namespace UnityTools
         {
             var parent = component.transform.parent;
 			items = parent.GetComponents<T>();
-
 			return items != null;
+        }
+
+        public static bool TryGetComponentInChildren<T>(this Component component, out T item)
+            where T : MonoBehaviour
+        {
+            item = component.GetComponentInChildren<T>();
+            return item != null;
+        }
+
+        public static bool TryGetComponentsInChildren<T>(this Component component, out T[] items)
+            where T : MonoBehaviour
+        {
+            items = component.GetComponentsInChildren<T>();
+            return items != null;
         }
     }
 }
